b70230a9eb934fafd95a973ce785a4b353dca5b4
Mike Fosmark <accounts@theginger.dk>
2017-09-26 12:11:41 +0200

    remove save user from index

--- index.php
+++ index.php
@@ -6,16 +6,6 @@ app::$conn  = $connection->open();
 
 $t = new User();
 
-$result = $t->save([
-	'email' => 'mike@theginger.dk',
-	'firstname' => 'Mike',
-	'lastname' => 'Fosmark',
-	'password' => 'test',
-	'nickname' => 'thegingerdk',
-]);
-
-app::p($result);
-
 // TODO: read and render templates and routes
 app::init();
 
