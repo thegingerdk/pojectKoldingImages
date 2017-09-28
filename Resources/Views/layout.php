b4212bd3c281d39ae5ffbda2ed0804e7dd99ea7d
DESKTOP-I36G5N2\ozols <ozols1992@otmail.com>
2017-09-28 14:16:44 +0200

    logo

--- Resources/Views/layout.php
+++ Resources/Views/layout.php
@@ -12,7 +12,7 @@
 	<?php if ( app::auth() ) { require_once "includes/upload.php"; } ?>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="#"><img src="assets/images/logo.png"</a>
+            <a class="navbar-brand" href="#"><img src="/assets/images/logo.png"</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
